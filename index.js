b8fb29d7eeb44f76a3985c2b42f3b52057c9f6ca
Laura Gonzalez <lg458@cornell.edu>
2022-07-04 00:38:09 -0700

    commit tree f71be47bc843484324c7ec6f67b52e053f8ed428

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-//creating employee object with name and address keys 
 const employee = { 
-  name: "Bob", 
-  address: "1234 Maple Street" 
+  name: "Laura", 
+  address: "1234 Maple St"
 } 
 
-//nondestructively updates employee object and returns a new employee object with the new key value pair
 function updateEmployeeWithKeyAndValue(employee, key, value) { 
-  return {
-    ...employee,
-    [key]: value,
-  };
+  return {...employee, [key]: value};
 } 
 
-//destructively updates employee with new key value pair by modifying original object and returns updated object
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) { 
   employee[key] = value; 
   return employee;
 } 
 
-//nondestructively deltes a key from a clone of employee and returns the new employee object 
 function deleteFromEmployeeByKey(employee, key) { 
   const newEmployee = {...employee}; 
   delete newEmployee[key]; 
   return newEmployee;
-} 
+}
 
-//destructively deletes key value pair and returns the original, now modified employee object
 function destructivelyDeleteFromEmployeeByKey(employee, key) { 
   delete employee[key]; 
   return employee;
